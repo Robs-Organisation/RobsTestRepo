593cba441ab16625fb44890f742191d769ca83a0
robin <robin.barzen@telekom.de>
2020-12-14 14:31:53 +0100

    added Main

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func add(varOne int, varTwo int) int {
+	return varOne + varTwo
+}
+
+func main() {
+
+	numberOne := 1
+	numberTwo := 2
+
+	result := add(numberOne, numberTwo)
+
+	fmt.Println("Result: ", result)
+
+}
